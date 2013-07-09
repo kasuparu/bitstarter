395fe9471027d4503df8fae85672c1711d50ee40
kasuparu <ticketing0@gmail.com>
2013-07-09 21:05:28 +0300

    modified:   .gitignore 	new file:   index.html 	modified:   web.js

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	fs.readFile('index.html', function(err, data){
+		if (err) throw err;
+		response.send(new Buffer(data).toString());
+	});
+	
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
